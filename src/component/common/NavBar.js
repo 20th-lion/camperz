27780f72dfa682a4a3aca7718d3136b7c0227004
hyunsu.An <ahs39490215@gmail.com>
2022-12-13 18:53:28 +0900

    feat: 하단 네비게이션바 구현
    버튼 클릭 시 지정된 url로 이동

---
+++ src/component/common/NavBar.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav>
+        <StyledUl>
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/chat/list">채팅</Link>
+          </li>
+          <li>
+            <Link to="/post/upload">게시글</Link>
+          </li>
+          <li>
+            <Link to="/profile">프로필</Link>
+          </li>
+        </StyledUl>
+      </nav>
+    </>
+  );
+}
+
+const StyledUl = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+`;
