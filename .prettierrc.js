c7b35adc741610c9b9991a4f0f811631c6e957a3
hyunsu.An <ahs39490215@gmail.com>
2022-12-13 11:22:15 +0900

    프로젝트 시작
    폴더구조 생성

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  printWidth: 120,
+  endOfLine: "auto",
+  singleQuote: true,
+  useTabs: true,
+  tabWidth: 8,
+  trailingComma: "all",
+  arrowParens: "always",
+};
