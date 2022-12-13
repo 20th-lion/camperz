d339b0205ae8b2e1c63c04d444b555aa326b448d
hyunsu.An <ahs39490215@gmail.com>
2022-12-13 21:31:22 +0900

    feat: 커스텀 axios 생성
    token값이 필요한것과 필요없는 헤더 구분해서 생성함

---
+++ src/lib/apis/axios.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://mandarin.api.weniv.co.kr';
+const TOKEN = localStorage.getItem('token');
+
+export const axiosPublic = axios.create({
+	baseURL: BASE_URL,
+	headers: {
+		'Content-type': 'application/json',
+	},
+});
+
+export const axiosPrivate = axios.create({
+	baseURL: BASE_URL,
+	headers: {
+		Authorization: `Bearer ${TOKEN}`,
+		'Content-type': 'application/json',
+	},
+});
