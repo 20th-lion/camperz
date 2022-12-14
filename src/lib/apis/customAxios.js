e0575383b59e595f8bdd68d17c483af53bb60749
eunha <dmsgk2323@naver.com>
2022-12-14 14:36:52 +0900

    refactor : env파일로 링크 이어지게 수정
    기존에 env 파일이 연결 안되던 문제를 해결하고 코드를 바꿨습니다.

--- src/lib/apis/customAxios.js
+++ src/lib/apis/customAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_PUBLIC_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 const TOKEN = localStorage.getItem('token');
 
 export const axiosPublic = axios.create({
--- src/lib/apis/postUploadApis.js
+++ src/lib/apis/postUploadApis.js
@@ -1,21 +1,16 @@
-// import { axiosPrivate } from './customAxios';
-
-// export const postUploader = async () => {
-// 	const reqPath = `/post`;
-// 	const exampleContent = {
-// 		post: {
-// 			content: String,
-// 			image: String, //"imageurl1, imageurl2" 형식으로
-// 		},
-// 	};
-// 	const res = await axiosPrivate.post(reqPath, exampleContent);
-// 	return res;
-// };
-
 import { axiosPrivate } from './customAxios';
 
 export const postUploader = async ({ postContent }) => {
 	const reqPath = `/post`;
-	const res = await axiosPrivate.post(`https://mandarin.api.weniv.co.kr${reqPath}`, postContent);
+
+	const res = await axiosPrivate.post(reqPath, postContent);
 	return res;
 };
+
+// import { axiosPrivate } from './customAxios';
+
+// export const postUploader = async ({ postContent }) => {
+// 	const reqPath = `/post`;
+// 	const res = await axiosPrivate.post(`https://mandarin.api.weniv.co.kr${reqPath}`, postContent);
+// 	return res;
+// };
