b71e389b4879b3a4376cf48c6bb6b852fa64b4cf
JuYeol Kim <112503881+jy1475@users.noreply.github.com>
2022-12-14 09:23:13 +0900

    refactor: App.js 수정
    불필요한 컴포넌트 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import PostUploadPage from './pages/PostUploadPage';
-import PostList from './post/PostList';
 
 export default function App() {
 	return (
 		<>
-			{/*<PostUploadPage/>*/}
-			<PostList />
+			
 		</>
 	);
 }
