5dc053121efee3938bf83ff784a92bd4d2e7ee79
hyunsu.An <ahs39490215@gmail.com>
2022-12-14 11:00:47 +0900

    feat: ProductItem 컴포넌트 구현
    List로 받아온 데이터의 각 요소를 화면에 렌더링합니다

--- src/component/product/ProductItem.js
+++ src/component/product/ProductItem.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function ProductItem() {
-	return <div></div>;
+export default function ProductItem({ itemName, price, itemImage, link, id }) {
+	return (
+		<>
+			<StyledItemBlock onClick={() => {}}>
+				<div>상품명{itemName}</div>
+				<div>가격{price}</div>
+				<div
+					style={{
+						width: '100px',
+						height: '100px',
+					}}
+				>
+					이미지
+					<Simg src={itemImage} alt="" />
+				</div>
+				<div>링크{link}</div>
+			</StyledItemBlock>
+		</>
+	);
 }
+
+const StyledItemBlock = styled.div`
+	border: 1px solid black;
+`;
+
+const Simg = styled.img`
+	width: 100%;
+	height: 100%;
+`;
