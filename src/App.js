eebc1826f72365f5827684762564c08d3dc8151d
hyunsu.An <ahs39490215@gmail.com>
2022-12-14 17:13:38 +0900

    refactor: 불필요한 파일 삭제, 오타수정
    useModals훅 삭제했습니다. App.js 오타 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import ProfilePage from './pages/Profile/ProfilePage';
+import ProductUploadPage from './pages/Product/ProductUploadPage';
 
 export default function App() {
-	return;
-	<>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/profile" element={<ProfilePage />}></Route>
-			</Routes>
-		</BrowserRouter>
-	</>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/profile" element={<ProfilePage />}></Route>
+					<Route path="/product" element={<ProductUploadPage />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
