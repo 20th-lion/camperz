26fd4b2b8ca9a5d16745968ebf140e2e3626c08a
itso-wavy <you.meet.vvavy@gmail.com>
2022-12-14 13:38:09 +0900

    refactor: splash에서 테스트코드 주석 삭제

--- src/pages/Home/Splash.js
+++ src/pages/Home/Splash.js
@@ -6,27 +6,11 @@ import { useNavigate } from "react-router-dom/dist";
 
 export default function Splash() {
   const navigate = useNavigate();
-  useEffect(()=>{
+  useEffect(() => {
     setTimeout(() => {
-      isLogin() ? navigate('/home'): navigate('/login')
+      isLogin() ? navigate('/home') : navigate('/login')
     }, 1500);
-  },[])
-
-/*   const Moving = () => {
-    useEffect(() => {
-    setTimeout(() => {
-      {
-        return (
-          <>
-            isLogin()
-            ? <HomePage />
-            : <LoginPage />
-          </>
-        )
-      }
-    }, 500);
-  }, []);
-} */
+  }, [])
 
   return (
     <>
