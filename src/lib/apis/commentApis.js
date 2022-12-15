5a59d38a1d169abfb7a2e8c86fcc55e0b866d54b
eunha <dmsgk2323@naver.com>
2022-12-15 16:56:07 +0900

    feat: 댓글 관련 api 리스트 작성 댓글 관련된 api들을 한 곳에 작성했습니다

---
+++ src/lib/apis/commentApis.js
@@ -0,0 +1,35 @@
+import { axiosPrivate } from './customAxios';
+
+//댓글 리스트 API
+export const getCommentList = async ({ post_id }) => {
+	const reqPath = `/post/:${post_id}/comments`;
+
+	const res = await axiosPrivate.get(reqPath);
+	return res;
+};
+
+//댓글 작성 API
+export const postComment = async ({ post_id, commentContent }) => {
+	const reqPath = `/post/:${post_id}/${commentContent}`;
+
+	const res = await axiosPrivate.post(reqPath, commentContent);
+	return res;
+};
+
+//댓글 삭제 API
+
+export const deleteComment = async ({ post_id, commentContent, comment_id }) => {
+	const reqPath = `/post/:${post_id}/${commentContent}/:${comment_id}`;
+
+	const res = await axiosPrivate.post(reqPath, commentContent);
+	return res;
+};
+
+//댓글 신고 API
+
+export const reportComment = async ({ post_id, commentContent, comment_id }) => {
+	const reqPath = `/post/:${post_id}/${commentContent}/:${comment_id}/report`;
+
+	const res = await axiosPrivate.post(reqPath, commentContent);
+	return res;
+};
--- src/pages/post/PostDetailPage.js
+++ src/pages/post/PostDetailPage.js
@@ -3,14 +3,15 @@ import PostDetailContent from '../../component/post/PostDetailContent';
 import PostDetailComment from '../../component/post/PostDetailComment';
 // import { postDetailLoader } from '../../lib/apis/postApis';
 
-export default function PostDetailPage() {
+//post 상세 페이지를 보려면 해당 페이지의 id가 필요하다.
+export default function PostDetailPage({ post_id }) {
 	// postDetailLoader(post_id).then((res) => {
 	// 	console.log(res);
 	// });
 	return (
 		<>
 			{/* <Headers /> */}
-			<PostDetailContent />
+			<PostDetailContent post_id={post_id} />
 			<PostDetailComment />
 		</>
 	);
