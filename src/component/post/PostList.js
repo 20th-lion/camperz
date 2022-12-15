917aee62b6cd91755575e16e762c0950f2a94bb1
hyunsu.An <112445899+HyunSu3949@users.noreply.github.com>
2022-12-15 21:53:58 +0900

    Update PostList.js
    fix: 오타 및 변수 수정

--- src/component/post/PostList.js
+++ src/component/post/PostList.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import PostItem from './PostItem';
 import { getPostList } from '../../lib/apis/postApis';
 
-export default function PostList(user) {
+export default function PostList({user}) {
 	const [postList, setPostList] = useState([]);
 
 	const loadPost = async () => {
-		await getPostList().then((res) => {
+		await getPostList(user).then((res) => {
 			setPostList([...res.data.post]);
 		});
 	};
