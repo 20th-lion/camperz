c89c5740167cf5392e32598e4103e6c7401b0965
hyunsu.An <ahs39490215@gmail.com>
2022-12-15 14:19:50 +0900

    refactor: productForm 생성
    컴포넌트 재사용을 위해 상품 입력폼 만들었습니다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import ProfilePage from './pages/Profile/ProfilePage';
 import ProductUploadPage from './pages/Product/ProductUploadPage';
+import ProfileEditPage from './pages/Profile/ProfileEditPage';
+import ProductEditPage from './pages/Product/ProductEditPage';
 
 export default function App() {
 	return (
@@ -9,7 +12,9 @@ export default function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/profile" element={<ProfilePage />}></Route>
+					<Route path="/account/edit" element={<ProfileEditPage />}></Route>
 					<Route path="/product" element={<ProductUploadPage />}></Route>
+					<Route path="/product/:id/edit" element={<ProductEditPage />}></Route>
 				</Routes>
 			</BrowserRouter>
 		</>
