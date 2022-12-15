357cfa7868fe18cc6e722ba10e8507342e72d92d
eunha <dmsgk2323@naver.com>
2022-12-15 11:39:04 +0900

    feat: 검색하기 버튼 구현 공통 컴포넌트인 검색하기 버튼을 구현했습니다

--- src/component/common/SearchButton.js
+++ src/component/common/SearchButton.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const LinkContainer = styled.div`
-	width: 102px;
-	height: 156px;
-`;
-
+const linktext = {
+	color: '#ffffff',
+	textDecoration: 'none',
+	width: '181px',
+	height: '34px',
+	display: 'block',
+	backgroundColor: '#5c6145',
+	textAlign: 'center',
+	paddingTop: '10px',
+};
 export default function searchButton() {
 	return (
 		<>
-			<Link to="/">검색하기</Link>
+			{/* //link에 있는 주소는 search 페이지로 이동 예정입니다.  */}
+
+			<Link to="/" style={linktext}>
+				검색하기
+			</Link>
 		</>
 	);
 }
