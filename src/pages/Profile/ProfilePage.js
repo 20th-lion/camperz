748d3b0cdacde4a3dd9a6f03ff42d08c31b62851
hyunsu.An <ahs39490215@gmail.com>
2022-12-16 13:45:28 +0900

    feat: 프로필 페이지 모달 함수 구현
    모달창 안에서 실행할 함수 구현하여 provider에 넘겨줌

--- src/pages/Profile/ProfilePage.js
+++ src/pages/Profile/ProfilePage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import Button from '../../component/common/Button';
 import NavBar from '../../component/common/NavBar';
@@ -8,14 +8,31 @@ import UserProfile from '../../component/user/UserProfile';
 import ProductList from '../../component/product/ProductList';
 import PostList from './../../component/post/PostList';
 
+import { logout } from '../../lib/utils/logout';
+import { ModalsDispatchContext } from '../../component/context/ModalsContext';
+import { modals } from './../../component/modal/Modals';
+
 export default function ProfilePage() {
 	const myAccountname = localStorage.getItem('accountname');
 	const { accountname } = useParams();
 	const user = accountname || myAccountname;
 
+	const { open } = useContext(ModalsDispatchContext);
+	const navigate = useNavigate();
+
+	const handleModalClick = () => {
+		open(modals.profileModal, {
+			onSetting: () => {},
+			onLogout: () => {
+				logout();
+				navigate('/');
+			},
+		});
+	};
+
 	return (
 		<>
-			<Header rightChild={<Button onClick={() => {}} text={'모달'} active />} />
+			<Header rightChild={<Button onClick={handleModalClick} text={'모달'} active />} />
 			<div>프로필</div>
 			<UserProfile type={accountname ? 'other' : 'mine'} user={user} />
 			<div>상품목록</div>
