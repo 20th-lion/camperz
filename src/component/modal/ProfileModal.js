4a317b9a1c4150881073dbd8949d29dba2b73030
hyunsu.An <ahs39490215@gmail.com>
2022-12-16 13:39:35 +0900

    feat: 프로필 페이지 모달 컴포넌트
    프로필 페이지 상단 모달 버튼 클릭시 나타남

--- src/component/modal/ProfileModal.js
+++ src/component/modal/ProfileModal.js
@@ -1,25 +1,24 @@
-import ReactModal from "react-modal";
-import { useNavigate } from "react-router-dom";
+import ReactModal from 'react-modal';
+import { useNavigate } from 'react-router-dom';
 
-export default function MyModal1({ onClose, onLogout, onSetting }) {
-  const navigate = useNavigate();
-  const handleClickSetting = () => {
-    onSetting();
-    onClose();
-  };
+export default function ProfileModal({ onClose, onLogout, onSetting }) {
+	const navigate = useNavigate();
+	const handleClickSetting = () => {
+		onSetting();
+		onClose();
+	};
 
-  const handleClickLogout = () => {
-    onLogout();
-    navigate("/");
-    onClose();
-  };
-  return (
-    <ReactModal isOpen ariaHideApp={false}>
-      <div>모달입니다</div>
-      <div>
-        <button onClick={handleClickSetting}>설정 및 개인정보</button>
-        <button onClick={handleClickLogout}>로그아웃</button>
-      </div>
-    </ReactModal>
-  );
+	const handleClickLogout = () => {
+		onLogout();
+		navigate('/');
+		onClose();
+	};
+	return (
+		<ReactModal isOpen ariaHideApp={false}>
+			<div>
+				<button onClick={handleClickSetting}>설정 및 개인정보</button>
+				<button onClick={handleClickLogout}>로그아웃</button>
+			</div>
+		</ReactModal>
+	);
 }
