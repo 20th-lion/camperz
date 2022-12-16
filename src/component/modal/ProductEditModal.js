0f4e9d06b7473026a96b2b2f52a23eb4fe4816c7
hyunsu.An <ahs39490215@gmail.com>
2022-12-16 13:35:46 +0900

    feat: 상품 클릭 모달 컴포넌트 구현
    상품 클릭시 나타나는 모달 컴포넌트

--- src/component/modal/ProductEditModal.js
+++ src/component/modal/ProductEditModal.js
@@ -1,33 +1,27 @@
-import ReactModal from "react-modal";
+import ReactModal from 'react-modal';
 
-export default function ProductEditModal({
-  onRemove,
-  onEdit,
-  onMove,
-  onClose,
-}) {
-  const handleClickRemove = () => {
-    onRemove();
-    onClose();
-  };
+export default function ProductEditModal({ onRemove, onEdit, onMove, onClose }) {
+	const handleClickRemove = () => {
+		onRemove();
+		onClose();
+	};
 
-  const handleClickEdit = () => {
-    onEdit();
-    onClose();
-  };
+	const handleClickEdit = () => {
+		onEdit();
+		onClose();
+	};
 
-  const handleClickMove = () => {
-    onMove();
-    onClose();
-  };
-  return (
-    <ReactModal isOpen ariaHideApp={false}>
-      <div>모달입니다</div>
-      <div>
-        <button onClick={handleClickRemove}>삭제하기</button>
-        <button onClick={handleClickEdit}>상품 수정하기</button>
-        <button onClick={handleClickMove}>상품 페이지로 이동</button>
-      </div>
-    </ReactModal>
-  );
+	const handleClickMove = () => {
+		onMove();
+		onClose();
+	};
+	return (
+		<ReactModal isOpen ariaHideApp={false}>
+			<div>
+				<button onClick={handleClickRemove}>삭제하기</button>
+				<button onClick={handleClickEdit}>상품 수정하기</button>
+				<button onClick={handleClickMove}>상품 페이지로 이동</button>
+			</div>
+		</ReactModal>
+	);
 }
