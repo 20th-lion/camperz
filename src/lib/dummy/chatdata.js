1e20072c22fa7fd5dbeb8b5b87e459527d3010c7
eunha <dmsgk2323@naver.com>
2022-12-19 16:31:48 +0900

    feat: 채팅방 라우터 설정

--- src/lib/dummy/chatdata.js
+++ src/lib/dummy/chatdata.js
@@ -1,5 +1,6 @@
 import profileImg from '../../assets/image/chat-profile.svg';
-export const chatData = [
+
+export const chatListData = [
 	{ author: { image: `${profileImg}`, userName: '캠핑왕' }, content: '그때 좋았다던 캠핑장이 어디지?' },
 	{ author: { image: `${profileImg}`, userName: '치키차카초코초' }, content: '주물팬 팔렸나요?' },
 	{
@@ -7,3 +8,12 @@ export const chatData = [
 		content: '2시 30분에 노랭상점 앞에서 뵙겠습니다!',
 	},
 ];
+
+// export const chatContentData = [
+// 	{ author: { image: `${profileImg}`, userName: '캠핑왕' }, content: '그때 좋았다던 캠핑장이 어디지?' },
+// 	{ author: { image: `${profileImg}`, userName: '치키차카초코초' }, content: '주물팬 팔렸나요?' },
+// 	{
+// 		author: { image: `${profileImg}`, userName: '자 이제 시작이야' },
+// 		content: '2시 30분에 노랭상점 앞에서 뵙겠습니다!',
+// 	},
+// ];
--- src/pages/Chat/ChatListPage.js
+++ src/pages/Chat/ChatListPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ChatList from '../../component/chat/ChatList';
 import NavBar from '../../component/common/NavBar';
 
 export default function ChatListPage() {
 	return (
 		<>
+			<ChatList />
 			<NavBar />
 		</>
 	);
