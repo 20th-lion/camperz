30a0c8ff23b183c01fd3fc26b7dbc443e9dba1f1
eunha <dmsgk2323@naver.com>
2022-12-19 17:29:40 +0900

    feat: commet 컴포넌트 왼쪽 이미지 기능 구현 comment 컴포넌트를 사용하는 컴포넌트에 따라 왼쪽 이미지를 props로 받아오는 기능 구현

--- src/pages/Chat/ChatListPage.js
+++ src/pages/Chat/ChatListPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ChatList from '../../component/chat/ChatList';
 import NavBar from '../../component/common/NavBar';
-
+import { Link } from 'react-router-dom';
 export default function ChatListPage() {
 	return (
 		<>
-			<ChatList />
+			<Link to={`/chatlist/1`}>
+				<ChatList />
+			</Link>
 			<NavBar />
 		</>
 	);
--- src/pages/Chat/ChatRoomPage.js
+++ src/pages/Chat/ChatRoomPage.js
@@ -1,5 +1,12 @@
 import React from 'react';
-
+import CommentBox from '../../component/common/CommentBox';
+import plusBtn from '../../assets/image/plus-button.svg';
 export default function ChatRoomPage() {
-	return <div>ChatRoomPage</div>;
+	const boxIcon = plusBtn;
+	return (
+		<>
+			안녕
+			<CommentBox boxIcon={boxIcon} />
+		</>
+	);
 }
