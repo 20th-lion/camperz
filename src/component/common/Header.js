5fe26e52ff52d97ebfe7d86a16f36cd2e15a0829
hyunsu.An <ahs39490215@gmail.com>
2022-12-21 11:14:21 +0900

    fix: 팔로워수 실시간 변화

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,5 +1,5 @@
 import BackButton from './BackButton';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export default function Header({ leftChild, rightChild }) {
 	return (
@@ -14,14 +14,14 @@ Header.defaultProps = {
 };
 
 const CommonHeader = styled.header`
-  height: 48px;
-  padding-left: 16px;
-  padding-right: 16px;
-  border-bottom: 1px solid #DBDBDB;
-  font-size: 18px;
-  font-weight: 400;
-  background-color: #F3F1E8;
-  display: flex;
-  justify-content: space-between ;
-  align-items: center;
+	height: 48px;
+	padding-left: 16px;
+	padding-right: 16px;
+	border-bottom: 1px solid #dbdbdb;
+	font-size: 18px;
+	font-weight: 400;
+	background-color: #f3f1e8;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
