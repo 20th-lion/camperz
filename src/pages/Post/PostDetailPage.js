ba38dd1a3155d9d77683152496b9decd242345be
eunha <dmsgk2323@naver.com>
2022-12-21 17:40:43 +0900

    feat: 포스트 디테일 헤더 수정 포스트 디테일 페이지 헤더를 아이콘 이미지로 수정했습니다.

--- src/pages/Post/PostDetailPage.js
+++ src/pages/Post/PostDetailPage.js
@@ -6,13 +6,15 @@ import PostDetailContent from '../../component/post/PostDetailContent';
 import PostDetailComment from '../../component/post/PostDetailComment';
 import CommentBox from '../../component/common/CommentBox';
 import boxIcon from '../../assets/icons/basic_profile_chat.png';
+import moreHeader from '../../assets/icons/more_header.png';
+import leftArrow from '../../assets/icons/icon_arrow_left.png';
 //post 상세 페이지를 보려면 해당 페이지의 id가 필요하다.
 export default function PostDetailPage() {
 	const { id } = useParams();
 
 	return (
 		<>
-			<Header leftChild={<h2>뒤로가기</h2>} rightChild={'점'} />
+			<Header leftChild={<img src={leftArrow} />} rightChild={<img src={moreHeader} />} />
 			<Main>
 				<PostDetailContent id={id} />
 				<PostDetailComment post_id={id} />
@@ -22,6 +24,7 @@ export default function PostDetailPage() {
 	);
 }
 
+const modalBtn = styled.img``;
 const Main = styled.main`
 	align-items: flex-start;
 	justify-content: flex-start;
