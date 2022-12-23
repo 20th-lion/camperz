7a9f6f81440dc4fa0cca2ea7e2a20a7d6751e978
itso-wavy <you.meet.vvavy@gmail.com>
2022-12-23 10:33:42 +0900

    feat: searchPage 스타일

--- src/pages/Search/SearchPage.js
+++ src/pages/Search/SearchPage.js
@@ -1,7 +1,57 @@
-export default function Search() {
+import { useState, useRef } from 'react';
+import Header from '../../component/common/Header';
+import NavBar from '../../component/common/NavBar';
+import BackButton from '../../component/common/BackButton';
+import SearchingResult from '../../component/search/SearchResult';
+import styled from 'styled-components';
+
+export default function SearchPage() {
+  const searchRef = useRef();
+  // const [account, setAccount] = useState();
+  const handleInputText = () => {
+
+  }
+
   return (
-    <div>
-      Search
-    </div>
-  )
+    <>
+      <Header
+        leftChild={
+          <><BackButton /><h2 className='ir'>검색페이지</h2></>}
+        rightChild={
+          <S_SearchingInput 
+            ref={searchRef} 
+            onChange={handleInputText}
+            placeholder='계정 검색' />}
+      />
+      <S_Main>
+        <ul>
+          <SearchingResult />
+          <SearchingResult />
+          <SearchingResult />
+        </ul>
+      </S_Main>
+      <NavBar page='home' />
+    </>
+  );
 }
+const S_SearchingInput = styled.input`
+  width: 316px;
+  height: 32px;
+  background: #FEFCF3;
+  border-radius: 32px;
+  padding-left: 16px;
+  font-size: 14px;
+  font-weight: 400;
+  ::placeholder {
+    font-weight: 300;
+  }
+`
+const S_Main = styled.main`
+  justify-content: flex-start;
+  ul {
+    margin: 20px 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+`
