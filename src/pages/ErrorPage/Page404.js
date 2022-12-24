ee492c9bbaefc90268ca66329a5d810bd22513f0
eunha <dmsgk2323@naver.com>
2022-12-24 15:51:43 +0900

    feat: 404페이지 스타일링

--- src/pages/ErrorPage/Page404.js
+++ src/pages/ErrorPage/Page404.js
@@ -3,17 +3,30 @@ import styled from 'styled-components';
 import iconSrc from '../../assets/icons/404_logo.png';
 import SearchButton from '../../component/common/SearchButton';
 
-const NotFoundImg = styled.img`
-	width: 102px;
-	height: 156px;
-`;
-
 export default function Page404() {
 	return (
 		<>
-			<NotFoundImg src={iconSrc} />
-			<div>페이지를 찾을 수 없습니다. :</div>
-			<SearchButton />
+			<S_Page404Wrapper>
+				<S_NotFoundImg src={iconSrc} />
+				<S_ErrorMsg>페이지를 찾을 수 없습니다. :(</S_ErrorMsg>
+				<SearchButton />
+			</S_Page404Wrapper>
 		</>
 	);
 }
+const S_Page404Wrapper = styled.div`
+	font-weight: 400;
+	font-size: 14px;
+	display: flex;
+	flex-direction: column;
+	margin: 0 auto;
+	align-items: center;
+	gap: 23px;
+`;
+const S_NotFoundImg = styled.img`
+	width: 102px;
+	height: 156px;
+`;
+const S_ErrorMsg = styled.p`
+	color: #767676;
+`;
