1d55a0d9d853996f018d209c61facb882cf810df
wavy_ <you.meet.vvavy@gmail.com>
2022-12-25 07:10:08 +0900

    Update searchApis.js

--- src/lib/apis/searchApis.js
+++ src/lib/apis/searchApis.js
@@ -5,11 +5,8 @@ export const getSearchApiResponse = async (inputs) => {
 	const loginData = {
 		user: inputs,
 	};
-	const headers = {
-		'Content-type': 'application/json',
-	};
 	try {
-		const res = await axiosPublic.post(reqPath, loginData, { headers });
+		const res = await axiosPublic.post(reqPath, loginData);
 		return res;
 	} catch (error) {
 		return error;
