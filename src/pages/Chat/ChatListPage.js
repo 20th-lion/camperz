1f885d7e575c6a501606027b5006aeadfbf2de12
hyunsu.An <ahs39490215@gmail.com>
2022-12-26 15:09:19 +0900

    remove: chat페이지 모달버튼 삭제

--- src/pages/Chat/ChatListPage.js
+++ src/pages/Chat/ChatListPage.js
@@ -4,12 +4,11 @@ import NavBar from '../../component/common/NavBar';
 import { Link } from 'react-router-dom';
 import Header from '../../component/common/Header';
 import styled from 'styled-components';
-import moreHeader from '../../assets/icons/more_header.png';
 
 export default function ChatListPage() {
 	return (
 		<>
-			<Header rightChild={<ModalBtn src={moreHeader} />} />
+			<Header />
 			<S_Main>
 				<Link to={`/chatlist/1`}>
 					<ChatList />
@@ -20,9 +19,6 @@ export default function ChatListPage() {
 	);
 }
 
-const ModalBtn = styled.img`
-	cursor: pointer;
-`;
 const S_Main = styled.main`
 	justify-content: flex-start;
 	align-items: flex-start;
