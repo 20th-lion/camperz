f675e254b2f75381344b653c344e8e9d07e2ec99
hyunsu.An <ahs39490215@gmail.com>
2022-12-26 17:53:06 +0900

    feat: 검색어 글자색 바꾸기 기능
    검색 내용에 따라 글씨색이 바뀝니다

--- src/component/search/SearchingItem.js
+++ src/component/search/SearchingItem.js
@@ -1,50 +1,70 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import defaultProfileImg from '../../assets/icons/basic_profile_chat.png'
+import defaultProfileImg from '../../assets/icons/basic_profile_chat.png';
 
-export default function SearchingItem({ image, username, accountname }) {
-  return (
-    <>
-      <li>
-        <Link to={'/profile/'+accountname}>
-          <S_A>
-            <S_ProfileImg src={image !== 'https://mandarin.api.weniv.co.kr/undefined' ? image : defaultProfileImg} />
-            <S_TextBox>
-              <S_Username>{username}</S_Username>
-              <S_AccountID>@ {accountname}</S_AccountID>
-            </S_TextBox>
-          </S_A>
-        </Link>
-      </li>
-    </>
-  )
+export default function SearchingItem({ image, username, accountname, keyword }) {
+	const ColoredItem = ({ item, query }) => {
+		return item.includes(query) ? (
+			<>
+				{item.split(query)[0]}
+				<S_ColoredSpan color={'#FF5E00'}>{keyword}</S_ColoredSpan>
+				{item.split(query)[1]}
+			</>
+		) : (
+			<>{item}</>
+		);
+	};
+	return (
+		<>
+			<li>
+				<Link to={'/profile/' + accountname}>
+					<S_A>
+						<S_ProfileImg src={image !== 'https://mandarin.api.weniv.co.kr/undefined' ? image : defaultProfileImg} />
+						<S_TextBox>
+							<S_Username>
+								<ColoredItem item={username} query={keyword} />
+							</S_Username>
+							<S_AccountID>
+								@ <ColoredItem item={accountname} query={keyword} />
+							</S_AccountID>
+						</S_TextBox>
+					</S_A>
+				</Link>
+			</li>
+		</>
+	);
 }
-
+const S_ColoredSpan = styled.span`
+	color: ${(props) => props.color || `none`};
+`;
 const S_A = styled.a`
-  width: 358px;
-  height: 58px;
-  display: flex;
-  padding: 4px 8px;
-  border-radius: 10px;
-  background: #FEFCF3;
-  cursor: pointer;
-`
+	width: 358px;
+	height: 58px;
+	display: flex;
+	padding: 4px 8px;
+	border-radius: 10px;
+	background: #fefcf3;
+	cursor: pointer;
+`;
 const S_ProfileImg = styled.img`
-  width: 50px;
-  height: 50px;
-`
+	width: 50px;
+	height: 50px;
+`;
 const S_TextBox = styled.div`
-  padding: 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 8px;
-`
+	padding: 15px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 8px;
+`;
 const S_Username = styled.p`
-  font-weight: 400;
-  font-size: 14px;
-`
+	font-weight: 400;
+	font-size: 14px;
+`;
 const S_AccountID = styled.p`
-  font-size: 12px;
-  color: #767676;
-` 
+	font-size: 12px;
+	color: #767676;
+`;
+const S_ColorSpan = styled.span`
+	color: red;
+`;
