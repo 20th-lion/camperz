4cb363cb57943645681821137c7a91aa5106c127
eunha <dmsgk2323@naver.com>
2022-12-29 15:32:47 +0900

    feat: footer css 스타일링 마무리

--- src/component/common/Footer.js
+++ src/component/common/Footer.js
@@ -29,26 +29,33 @@ export default function Footer() {
 
 	return (
 		<>
-			<S_Footer>
+			<S_Footer role="banner">
 				<S_Links>
 					<Link to={'/search'}>
-						<strong>유저 검색 하기</strong>
+						<S_LinkStrong>유저 검색 하기</S_LinkStrong>
 					</Link>
-					<strong onClick={handleModalClick} style={{ cursor: 'pointer' }}>
+					<S_ButtonLogout onClick={handleModalClick} style={{ cursor: 'pointer' }} aria-label="로그아웃 버튼">
 						로그아웃
-					</strong>
+					</S_ButtonLogout>
+
 					<Link to={'/profile/edit'}>
-						<strong>회원 정보 수정</strong>
+						<S_LinkStrong>회원 정보 수정</S_LinkStrong>
 					</Link>
 				</S_Links>
 				<S_ContactWrapper>
-					<S_LogoBox src={logoImg} />
+					<S_LogoBox src={logoImg} alt="캠퍼즈 로고 이미지 입니다" />
 					<S_ContactBox>
-						<S_ContactInfo>E-mail : tomato@gmail.com</S_ContactInfo>
+						<S_ContactInfo>
+							<address>E-mail : tomato@gmail.com</address>
+						</S_ContactInfo>
 						<S_ContactInfo>서비스 이용 약관 개인 정보 처리 방침</S_ContactInfo>
 					</S_ContactBox>
 					<a href="https://github.com/20th-lion/camperz" target="_blank">
-						<S_GitHub src={githubImg} />
+						<S_GitHub
+							src={githubImg}
+							alt="GitHub 로고 이미지 입니다. 누르면 캠퍼즈 깃허브 페이지로 이동합니다."
+							title=" 사이트 GitHub 페이지로 이동하기"
+						/>
 					</a>
 				</S_ContactWrapper>
 				<S_CopyrightWrapper>© 2022 CAMPERZ from LikeLion 3th All right reserved.</S_CopyrightWrapper>
@@ -60,6 +67,7 @@ export default function Footer() {
 const S_Footer = styled.footer`
 	margin: 0 auto;
 	margin-top: 100px;
+	margin-bottom: 60px;
 	padding: 10px;
 	width: 317px;
 
@@ -73,6 +81,20 @@ const S_Links = styled.div`
 	font-size: 10px;
 	justify-content: space-around;
 	margin-top: 10px;
+	vertical-align: bottom;
+	padding: 0;
+`;
+
+const S_LinkStrong = styled.strong`
+	vertical-align: middle;
+	padding: 0;
+`;
+
+const S_ButtonLogout = styled.button`
+	font-size: 10px;
+	color: #767676;
+	font-family: 'Noto Sans KR', sans-serif;
+	font-weight: 300;
 `;
 
 const S_ContactWrapper = styled.div`
