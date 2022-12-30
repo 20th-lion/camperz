a424ad08c969de5677d31021a8d2646feda8d16d
itso-wavy <you.meet.vvavy@gmail.com>
2022-12-30 23:27:01 +0900

    fix: 레이아웃 오류 수정

--- src/pages/Chat/ChatListPage.js
+++ src/pages/Chat/ChatListPage.js
@@ -25,19 +25,13 @@ export default function ChatListPage() {
 const S_Main = styled.main`
 	height: fit-content;
 	min-height: calc(100vh - 108px);
-	margin: 20px auto 0;
 	overflow-y: visible;
 `;
-
 const S_Wapper = styled.div`
-	word-break: break-all;
 	height: calc(100vh - 108px);
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
-	/* align-items: center; */
-
-	overflow-x: hidden;
 	overflow-y: scroll;
 	-ms-overflow-style: none;
 	scrollbar-width: none;
--- src/pages/Profile/ProfilePage.js
+++ src/pages/Profile/ProfilePage.js
@@ -11,77 +11,52 @@ import { modals } from './../../component/modal/Modals';
 import { useModals } from './../../lib/hooks/useModals';
 import moreIcon from '../../assets/icons/more_header.png';
 import { LoginDispatchContext } from '../../component/context/LoginContext';
-import Footer from '../../component/common/Footer';
 
 export default function ProfilePage() {
-	const { logout } = useContext(LoginDispatchContext);
-	const myAccountname = localStorage.getItem('accountname');
-	const { accountname } = useParams();
-	const user = accountname || myAccountname;
-	const type = user === myAccountname ? 'mine' : 'other';
+  const { logout } = useContext(LoginDispatchContext);
+  const myAccountname = localStorage.getItem('accountname');
+  const { accountname } = useParams();
+  const user = accountname || myAccountname;
+  const type = user === myAccountname ? 'mine' : 'other';
 
-	const { openModal } = useModals();
-	const navigate = useNavigate();
+  const { openModal } = useModals();
+  const navigate = useNavigate();
 
-	const handleModalClick = () => {
-		openModal(modals.profileModal, {
-			onSetting: () => {},
-			onLogout: () => {
-				openModal(modals.confirmModal, {
-					onConfirm: () => {
-						logout();
-						navigate('/');
-					},
-					message: '로그아웃 하시겠어요?',
-					btnText: '로그아웃',
-				});
-			},
-		});
-	};
+  const handleModalClick = () => {
+    openModal(modals.profileModal, {
+      onSetting: () => { },
+      onLogout: () => {
+        openModal(modals.confirmModal, {
+          onConfirm: () => {
+            logout();
+            navigate('/');
+          },
+          message: '로그아웃 하시겠어요?',
+          btnText: '로그아웃',
+        });
+      },
+    });
+  };
 
-	return (
-		<>
-			<Header rightChild={<S_IconImg onClick={handleModalClick} src={moreIcon} />} />
-			<h2 className="ir">프로필 페이지</h2>
-			<S_Wapper>
-				<S_Main>
-					<UserProfile user={user} type={type} />
-					<ProductList user={user} type={type} />
-					<PostList user={user} type={type} />
-				</S_Main>
-				<Footer />
-			</S_Wapper>
-			<NavBar page={type === 'mine' ? 'user' : 'home'} />
-		</>
-	);
+  return (
+    <>
+      <Header rightChild={<S_IconImg onClick={handleModalClick} src={moreIcon} />} />
+      <h2 className="ir">프로필 페이지</h2>
+      <S_Main>
+        <UserProfile user={user} type={type} />
+        <ProductList user={user} type={type} />
+        <PostList user={user} type={type} />
+      </S_Main>
+      <NavBar page={type === 'mine' ? 'user' : 'home'} />
+    </>
+  );
 }
 
 const S_Main = styled.main`
-	background-color: #f2f2f2;
-	height: fit-content;
-	/* min-height: calc(100vh - 108px); */
-	margin: 20px auto 0;
-	overflow-y: visible;
-`;
+  background-color: #f2f2f2;
+`
 const S_IconImg = styled.img`
 	width: 24px;
 	height: 24px;
 	cursor: pointer;
-`;
-
-const S_Wapper = styled.div`
-	word-break: break-all;
-	height: calc(100vh - 108px);
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	/* align-items: center; */
-
-	overflow-x: hidden;
-	overflow-y: scroll;
-	-ms-overflow-style: none;
-	scrollbar-width: none;
-	&::-webkit-scrollbar {
-		display: none;
-	}
 `;
