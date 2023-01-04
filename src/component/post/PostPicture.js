e5c3d05f4739d8bb4ab274135006ac46860a59e5
hyunsu.An <ahs39490215@gmail.com>
2023-01-04 13:47:18 +0900

    fix: 사진만 보기 에러 이미지 처리

--- src/component/post/PostPicture.js
+++ src/component/post/PostPicture.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import postAltImg from '../../assets/image/post_alt_img.png';
+
 export default function PostPicture({ id, image }) {
 	const navigate = useNavigate();
 	const handleImgClick = () => {
 		navigate(`/postdetail/${id}`);
 	};
+
+	const handleErrorImg = (e) => {
+		e.target.src = postAltImg;
+	};
 	return (
 		<>
 			{image && (
 				<div onClick={handleImgClick}>
-					<img src={image} alt="포스트 이미지" />
+					<img src={image} alt="포스트 이미지" onError={handleErrorImg} />
 				</div>
 			)}
 		</>
